bf8d8b831c60c583d877842f7d0216be6d774f56
bizbaeja <bjgbjg112@gmail.com>
2023-07-05 15:22:55 +0900

    initial commit

---
+++ src/components/callChatGptAPI.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+async function callChatGptAPI(question) {
+
+    // ChatGPT API 호출 함수
+{
+    // ChatGP API 호출 코드 작성
+    // question을 API에 전달하여 응답을 받아옵니다.
+  }
+  
+}
+
+export default callChatGptAPI

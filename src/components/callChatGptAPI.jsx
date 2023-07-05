86be28e7a0f14d071f7b8212e416f085a096ee86
bizbaeja <bjgbjg112@gmail.com>
2023-07-05 15:32:18 +0900

    callAPI

--- src/components/callChatGptAPI.jsx
+++ src/components/callChatGptAPI.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
 
+import axios from 'axios';
 async function callChatGptAPI(question) {
 
-    // ChatGPT API 호출 함수
-{
-    // ChatGP API 호출 코드 작성
-    // question을 API에 전달하여 응답을 받아옵니다.
+  try {
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      prompt: question,
+      max_tokens: 50,
+      temperature: 0.7,
+      n: 1
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': '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' // ChatGPT API 키를 입력하세요.
+      }
+    });
+
+    return response.data.choices[0].text.trim();
+  } catch (error) {
+    console.error(error);
+    return '';
   }
-  
 }
 
 export default callChatGptAPI
